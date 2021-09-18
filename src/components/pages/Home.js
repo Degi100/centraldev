a43bbe89e4b43fbca7c8b19a6f483ff7321f3eb1
degi100 <r.degering@mail.de>
2021-09-18 16:18:52 +0200

    change layer responsiv

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./Home.scss";
-import { ExternalLink } from 'react-external-link';
+import { ExternalLink } from "react-external-link";
 
-
-export const Home = () => { 
+export const Home = () => {
   return (
     <div>
       <div className="layer1">
@@ -13,22 +12,40 @@ export const Home = () => {
       <div className="layer2">
         <h1>GitHub Links</h1>
         <div>
-          <ExternalLink href="https://www.github.com/Degi100"><spwan>My GitHub</spwan></ExternalLink>
-          <ExternalLink href="https://github.com/FbW-D01"><spwan>D01 Github</spwan></ExternalLink>
-          <ExternalLink href="https://github.com/Entwickler-Club/dpodreact">Datapod Project</ExternalLink>
+          <ExternalLink href="https://www.github.com/Degi100">
+            <span>My GitHub</span>
+          </ExternalLink>
+          <ExternalLink href="https://github.com/FbW-D01">
+            <span>D01 Github</span>
+          </ExternalLink>
+          <ExternalLink href="https://github.com/Entwickler-Club/dpodreact">
+            <span>Datapod Project</span>
+          </ExternalLink>
         </div>
       </div>
+      
       <div className="layer2">
-        <h1>Projects</h1>          
-        <ExternalLink href="https://vimninja.netlify.app/"><spwan>VIM NINJA CSS LAYOUT</spwan></ExternalLink>
-          <ExternalLink href="https://degidev.netlify.app/"><spwan>My HTML/ CSS Portfolio</spwan></ExternalLink>
-          <ExternalLink href="https://energycalc.netlify.app/"><spwan>Energy Calculator with VueJS</spwan></ExternalLink>
+        <h1>Projects</h1>
+        <ExternalLink href="https://vimninja.netlify.app/">
+          <span>VIM NINJA CSS LAYOUT</span>
+        </ExternalLink>
+        <ExternalLink href="https://degidev.netlify.app/">
+          <span>My HTML/ CSS Portfolio</span>
+        </ExternalLink>
+        <ExternalLink href="https://energycalc.netlify.app/">
+          <span>Energy Calculator with VueJS</span>
+        </ExternalLink>
       </div>
+
       <div className="layer2">
         <h1>External Links</h1>
-              <ExternalLink href="https://onespace.netlify.app/"><spwan>OneSpace by Edward T.</spwan></ExternalLink>
-          <ExternalLink href="https://d01.site/"><spwan>D01-Site</spwan></ExternalLink>
-  </div>
+        <ExternalLink href="https://onespace.netlify.app/">
+          <span>OneSpace by Edward T.</span>
+        </ExternalLink>
+        <ExternalLink href="https://d01.site/">
+          <span>D01-Site</span>
+        </ExternalLink>
+      </div>
     </div>
   );
 };
