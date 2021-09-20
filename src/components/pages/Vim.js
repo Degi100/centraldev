e60b28e78d69480ca72aa4c6ea0f21acad5aa3a0
Degi100 <r.degering@mail.de>
2021-09-21 01:25:47 +0200

    change

--- src/components/pages/Vim.js
+++ src/components/pages/Vim.js
@@ -1,22 +1,28 @@
 import React from "react";
 import vimninjabelts from "../../data/vimninjabelts.json";
 import ReactPlayer from 'react-player'
+import { RiMovieLine } from "react-icons/ri"
 import "./vim.scss";
 
 export const Vim = () => {
   return (
     <div>
       <div className="layer-headline">
-        <h1>Vim Ninja </h1>
+        <h1>Vim Ninja  <input
+          type="text"
+           placeholder="Search VIM "
+        /> </h1>
+       
+
       </div>
       <div className="layerWhite">
         <div className="vimninjabelts">
           {vimninjabelts.white.map((belt) => {
             return (
               <div className={`vimTitle`}>
-                <p className="title">{belt.title}</p>
+                <p className="title">{belt.title} </p>
                 <p className="command">{belt.command}</p>
-                <p className="player"><ReactPlayer url={belt.tutorial} /></p>
+                <p className="player"><ReactPlayer url={belt.tutorial}/> </p>
               </div>
             );
           })}
