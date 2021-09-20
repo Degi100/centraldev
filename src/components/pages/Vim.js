c44ffdc972f2ddf8f07b0304ec8cafa2d051d687
degi100 <r.degering@mail.de>
2021-09-20 19:44:43 +0200

    edit vim

--- src/components/pages/Vim.js
+++ src/components/pages/Vim.js
@@ -4,29 +4,36 @@ import "./vim.scss";
 
 export const Vim = () => {
   return (
-    <>
+    <div>
       <div className="layer-headline">
         <h1>Vim Ninja </h1>
-        </div>
-        <div className="layer2">
-        
+      </div>
+      <div className="layerWhite">
         <div className="vimninjabelts">
           {vimninjabelts.white.map((belt) => {
             return (
-              <>
-                <div className={`vimTitle`}>
-                  <p>{belt.title}</p>
-                  <p>{belt.command}</p>
-                  <p>{belt.tutorial}</p>
-                   <p>{belt.howto}</p>
-               
-                    </div>
-  
-              </>
+              <div className={`vimTitle`}>
+                <p>{belt.title}</p>
+                <p className="command">{belt.command}</p>
+              </div>
             );
           })}
         </div>
+      </div>
+
+      <div className="layerYellow">
+        <div className="vimninjabelts">
+          {vimninjabelts.yellow.map((belt) => {
+            return (
+              <div className={`vimTitle`}>
+                <p>{belt.title}</p>
+                <p className="command">{belt.command}</p>
+              </div>
+            );
+          })}
+          <div className="flex"></div>
         </div>
-      </>
+      </div>
+    </div>
   );
 };
